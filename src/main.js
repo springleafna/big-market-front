36e7290a36800e718e49ba9c9e9618a37a1800ef
springleaf <3313252209@qq.com>
2024-12-18 18:54:03 +0800

    feat:241218-init project

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import VueLuckyCanvas from '@lucky-canvas/vue'
+
+const app = createApp(App)
+
+app.use(router)
+app.use(VueLuckyCanvas)
+
+app.mount('#app')
